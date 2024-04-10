77c643e9f20a772fe583fffb87ea67e6e7618156
JKermany <kermanycosta@gmail.com>
2024-04-09 21:04:14 -0300

    Resposta

---
+++ src/main/java/br/ufrn/imd/Person.java
@@ -0,0 +1,20 @@
+package br.ufrn.imd;
+
+public class Person {
+
+    String name;
+    String hometown;
+
+    public Person(){
+
+    }
+
+    public String name() {
+        return name;
+    }
+
+    public String hometown() {
+        return hometown;
+    }
+
+}
